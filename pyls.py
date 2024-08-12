6759e4d22d77f99442bde67e64063583bffca84a
harshdixit05 <harsh_dixit.sias22@Krea.ac.in>
2024-08-12 22:27:14 +0530

    Implement file type and detailed listing combination. Closes #4

--- pyls.py
+++ pyls.py
@@ -61,3 +61,62 @@ def getLongFormatResults(results):
         filename = i["filename"]
         list_result.append(modtime + "\t" + filesize + "\t" + filename)
     return list_result
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="pyls",
+    description="Lists files in given or current directory",
+    epilog="Poor man's ls",
+)
+
+parser.add_argument(
+    "dirname",
+    help="Name of directory to list the contents of",
+    action="store",
+    nargs="?",
+    default=".",
+)
+
+parser.add_argument(
+    "-F",
+    "--filetype",
+    help="""Adds an extra character to the end of the printed
+                            filename that indicates its type.""",
+    action="store_true",
+)
+
+parser.add_argument(
+    "-l",
+    "--long-format",
+    help="Presents more details about files in columnar format",
+    action="store_true",
+)
+
+args = parser.parse_args()
+
+def main(arguments):
+    """
+    Processes the whole program and assigns it to functions
+    :param arguments:
+    """
+    lines = getDescriptionsOfFilesInDir(arguments.dirname)
+    result_list = formatResults(lines, arguments.long_format, arguments.filetype)
+    displayResults(result_list)
+
+def displayResults(lines):
+    """
+    Takes a list of lines and prints them all to the standard output.
+
+    Input:
+    lines = List of strings
+
+    Output:
+    On standard output
+    """
+    assert isinstance(lines, list), "lines should be a list"
+
+    for line in lines:
+        print(line)
+
+if __name__ == '__main__':
+    main(args)
