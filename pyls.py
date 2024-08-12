cbf6d4e857091d2063372abdfa7d378959f56f92
harshdixit05 <harsh_dixit.sias22@Krea.ac.in>
2024-08-12 22:24:12 +0530

    Add detailed listing with date and size. Closes #3

--- pyls.py
+++ pyls.py
@@ -41,3 +41,23 @@ def formatResultsWithFileType(results):
             filename = i["filename"]
         list_result.append(filename)
     return list_result
+def getLongFormatResults(results):
+    """
+    Takes a list of file descriptions and returns a list of formatted strings
+    with detailed information including last modified date, file size, and filename.
+
+    Inputs -
+    results = List of dictionaries, like returned by getDescriptionsOfFilesInDir()
+
+    Outputs:
+    List of strings in the format: "YYYY-MM-DD HH:MM:SS <filesize> <filename>"
+    """
+    assert isinstance(results, list), "results should be a list"
+
+    list_result = []
+    for i in results:
+        filesize = str(i["filesize"])
+        modtime = str(i["modtime"])
+        filename = i["filename"]
+        list_result.append(modtime + "\t" + filesize + "\t" + filename)
+    return list_result
